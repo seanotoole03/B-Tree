42cb4e5df4bee25042edb7679a81cbc22a0bcc4d
AngelSanabria415 <angelsanabria415@gmail.com>
2018-11-16 18:29:53 -0700

    Add files via upload

--- BTree.java
+++ BTree.java
@@ -2,6 +2,8 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+
 import javax.xml.soap.Node;
 
 /**
@@ -122,10 +124,15 @@ public class BTree<T extends Comparable>
 	   }
 	
 	
+	//test a root with two children.  Write  a value into node into memory.
+	//Array of key values for the node, create the root, how 
+	
+	//judge the pointers with the size of node, based on degree
+	
 	/**
 	 * @param x
 	 */
-	public void splitChild(BTreeNode x, BTreeNode y, int i)
+	public void splitChild(BTreeNode x, BTreeNode y, int i)// splitting will be the only time where we have pointers
 	 {
 	    BTreeNode z = new BTreeNode();
         z.setIsLeaf(y.isLeaf());
@@ -208,5 +215,201 @@ public class BTree<T extends Comparable>
 	{
 		return height;
 	}
+	
+	//Inner Class BTreeNode
+		/**
+		 * This class
+		 * 
+		 * @author angelsanabria
+		 *
+		 */
+		public class BTreeNode 
+		{
+			    private int n; // number of key-value pairs in the BTree
+			    private LinkedList<TreeObject> keys;
+			    private LinkedList<Integer> children;
+			    private int parent;
+			    private int offset;
+			    private boolean isLeaf;
+			    
+			    /**
+			     * 
+			     */
+			    public BTreeNode()
+			    {
+			        parent = -1;
+			        keys = new LinkedList<TreeObject>();
+			        children = new LinkedList<Integer>();
+			        n = 0;
+			    }
+			    
+			    /**
+			     * 
+			     * 
+			     * @param parent
+			     */
+			    public void setParent(int parent)
+			    {
+			        this.parent = parent;
+			    }
+			    
+			    /**
+			     * 
+			     * 
+			     * @return
+			     */
+			    public int getParent()
+			    {
+			        return parent;
+			    }
+			    
+			    /**
+			     * @param n
+			     */
+			    public void addChild(int n)
+			    {
+			        children.add(n);
+			    }
+			    
+			    /**
+			     * @param x
+			     * @param i
+			     */
+			    public void addChild(Integer x, int i)
+			    {
+			        children.add(i,x);
+			    }
+			    
+			    /**
+			     * @param i
+			     * @return
+			     */
+			    public int removeChild(int i)
+			    {
+			        return children.remove(i);
+			    }
+			    
+			    /**
+			     * @param i
+			     * @return
+			     */
+			    public int getChild(int i)
+			    {
+			        return children.get(i).intValue();
+			    }
+			    
+			    /**
+			     * 
+			     * 
+			     * @return
+			     */
+			    public LinkedList<Integer> getChildren()
+			    {
+			        return children;
+			    }
+			    
+			    /**
+			     * This method gets the number of key-value pairs in the BTree
+			     * 
+			     * @return
+			     */
+			    public int getN()
+			    {
+			    	return n;
+			    }
+			    
+			    /**
+			     * This method sets the number of key-value pairs in the BTree
+			     * 
+			     * @param i
+			     */
+			    public void setN(int i)
+			    {
+			    	n = i;
+			    }
+			    
+			    /**
+			     * This method gets the key value
+			     * 
+			     * @param k
+			     * @return
+			     */
+			    public TreeObject getKey(int k)
+			    {
+			        TreeObject obj = keys.get(k);
+			        return obj;
+			    }
+			    
+			    /**
+			     * 
+			     * 
+			     * @param obj
+			     */
+			    public void addKey(TreeObject obj)
+			    {
+			        keys.add(obj);
+			    }
+			    
+			    /**
+			     * @param obj
+			     * @param i
+			     */
+			    public void addKey(TreeObject obj, int i)
+			    {
+			        keys.add(i,obj);
+			    }
+			    
+			    /**
+			     * This method gets the offset
+			     * 
+			     * @return
+			     */
+			    public int getOffset()
+			    {
+			    	return offset;
+			    }
+			    
+			    /**
+			     * This method sets the offset
+			     * 
+			     * @param offset
+			     * @return offset
+			     */
+			    public int setOffset(int offset)
+			    {
+			    	return this.offset = offset;
+			    }
+			    
+			    /**
+			     * This method sets a leaf from a branch
+			     * 
+			     * @param isLeaf
+			     */
+			    public void setIsLeaf(boolean isLeaf)
+			    {
+			        this.isLeaf = isLeaf;
+			    }
+			    
+			    /**
+			     * This method checks if its a leaf
+			     * 
+			     * @return true or false
+			     */
+			    public boolean isLeaf()
+			    {
+			    	return isLeaf();
+			    }
+			    
+			    /**
+			     * This method removes the key
+			     * 
+			     * @param i
+			     * @returns the removed key
+			     */
+			    public TreeObject removeKey (int i)
+			    {
+			    	return keys.remove(i);
+			    }
+		}
 	
 }
